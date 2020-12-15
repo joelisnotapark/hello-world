84406eb02284c1bd0e48315f5b06735d892ca2d0
joelisnotapark <74550350+joelisnotapark@users.noreply.github.com>
2020-12-15 09:22:53 -0500

    Create codingbat_list2.py

---
+++ codingbat_list2.py
@@ -0,0 +1,67 @@
+# count_evens
+def count_evens(nums):
+    count = 0
+  
+    for i in range(0, len(nums), 1):
+        if (nums[i] % 2 == 0):
+            count = count + 1
+  
+    return count
+    
+    
+# big_diff
+def big_diff(nums):
+    maximum = nums[0]
+    minimum = nums[0]
+
+    for i in range(1,len(nums)):
+        maximum = max(maximum, nums[i])
+        minimum = min(minimum, nums[i])
+
+    return maximum - minimum
+    
+    
+# centered_average
+def centered_average(nums):
+    return (sum(nums) - max(nums) - min(nums)) / (len(nums) - 2)
+    
+   
+# sum13
+def sum13(nums):
+    x = 0
+    i = 0
+
+    while i < len(nums):
+        if nums[i] == 13:
+            i += 1
+        else:
+            x += nums[i]
+        i += 1
+
+    return x
+    
+    
+# sum67
+def sum67(nums):
+    sum = 0
+    flag = False
+
+    for i in range(len(nums)):
+        if nums[i] == 6:
+            flag = True
+        if not flag:
+            sum += nums[i]
+        if nums[i] == 7 and flag:
+            flag = False
+
+    return sum
+    
+    
+# has22
+def has22(nums):
+    for i in range(len(nums)-1):
+        if nums[i] == 2 and nums[i+1] == 2:
+            return True
+                
+    return False
+
